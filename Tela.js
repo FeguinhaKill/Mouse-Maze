34918c91949529d2066bd433f4a0221afdd48de1
FeguinhaKill <132961042+FeguinhaKill@users.noreply.github.com>
2025-04-10 08:19:41 -0300

    Update and rename Js.txt to Tela.js

---
+++ Tela.js
@@ -0,0 +1,51 @@
+const tela = document.getElementById('labrintotela');
+const conteudo = tela.getContext('2d');
+
+let Labirinto = [
+    [1, 3, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0, 1],
+    [1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1],
+    [1, 0, 1, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 0, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1],
+    [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 1],
+    [1, 0, 1, 0, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 0, 1, 1, 0, 1],
+    [1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1],
+    [1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1],
+    [1, 0, 0, 0, 0, 1, 2, 1, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 1],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+];
+const tamanho = 30;
+
+// Função para desenhar o labirinto
+function drawLabirinto() {
+    for (let linha = 0; linha < Labirinto.length; linha++) {
+        for (let coluna = 0; coluna < Labirinto[linha].length; coluna++) {
+            if (Labirinto[linha][coluna] === 1) {
+                conteudo.fillStyle = 'black';
+                conteudo.fillRect(coluna * tamanho, linha * tamanho, tamanho, tamanho);
+            } else if (Labirinto[linha][coluna] === 0) {
+                conteudo.fillStyle = 'white';
+                conteudo.fillRect(coluna * tamanho, linha * tamanho, tamanho, tamanho);
+            } else if (Labirinto[linha][coluna] === 2) {
+                conteudo.fillStyle = 'green';
+                conteudo.fillRect(coluna * tamanho, linha * tamanho, tamanho, tamanho);
+            } else if (Labirinto[linha][coluna] === 3) {
+                conteudo.fillStyle = 'blue';
+                conteudo.fillRect(coluna * tamanho, linha * tamanho, tamanho, tamanho);
+            }
+        }
+    }
+}
+
+// Inicia o jogo
+drawLabirinto();
